6ae69ed55678f884ba5e7904ac6dec00e9cf7708
BABATUNDE IYANUOLUWA <babatundeiyanuoluwa1@gmail.com>
2021-05-11 13:35:19 +0100

    New Update

--- music script.js
+++ music script.js
@@ -3,16 +3,16 @@ const playBtn = document.querySelector('#play')
 const prevBtn = document.querySelector('#prev')
 const nextBtn = document.querySelector('#next')
 const audio = document.querySelector('#audio')
-const progress = document.querySelector('.progresss')
+const progress = document.querySelector('.progress')
 const progressContainer = document.querySelector('.progress-container')
 const title = document.querySelector('#title')
 const cover = document.querySelector('#cover')
 
 //Song titles
-const songs = ['Shawn Mendes-Cold In', 'Shawn Mendes-Nothing Holding Me Back', 'Fill my cup Lord']
+const songs = ['Shawn Mendes-Cold In', 'Shawn Mendes-Nothing Holding Me Back', 'Barcelona']
 
 //keep track of songs
-let songIndex = 2
+let songIndex = 1
 
 //Intially load song info DOM
 loadSong(songs[songIndex])
@@ -20,8 +20,8 @@ loadSong(songs[songIndex])
 //Update song details
 function loadSong(song) {
     title.innerText = song
-    audio.src= `C:/Projects/Music Player${song}.mp3`
-    cover.src= `C:/Projects/Music Player/${song}.jpg`
+    audio.src= `music/${song}.mp3`
+    cover.src= `images/${song}.jpg`
 }
 
 function playSong() {
@@ -40,6 +40,46 @@ function pauseSong() {
     audio.pause()
 }
 
+function prevSong() {
+    songIndex --
+
+    if (songIndex < 0) {
+    songIndex = songs.length - 1
+}
+
+    loadSong(songs[songIndex])
+    playSong()
+}
+
+function nextSong() {
+    songIndex ++
+
+    if (songIndex > songs.length - 1 ) {
+    songIndex = 0
+}
+
+    loadSong(songs[songIndex])
+    playSong()
+    
+}
+
+function updateProgress(e) {
+    const {duration, currentTime} = e.srcElement
+    const progressPercent = (currentTime / duration) * 100
+    progress.style.width= `${progressPercent}%`
+
+}
+
+
+function setProgress(e) {
+    const width = this.clientWidth
+    const clickX = e.offsetX
+    const duration = audio.duration
+
+    audio.currentTime = (clickX / width) * duration
+}
+    
+
 //Event listeners
 playBtn.addEventListener('click', () => {
     const isPlaying = musicContainer.classList.contains('play')
@@ -50,3 +90,13 @@ playBtn.addEventListener('click', () => {
         playSong()
     }
 })
+
+//Change song event listeners
+prevBtn.addEventListener('click', prevSong)
+nextBtn.addEventListener('click', nextSong)
+
+audio.addEventListener('timeupdate', updateProgress)
+
+progressContainer.addEventListener('click', setProgress)
+
+audio.addEventListener('ended', nextSong)
